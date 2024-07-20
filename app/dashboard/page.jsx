5b52da4f4e5eacfd0fe6a9ac99238761ecde29d3
Ved8290 <133479514+Ved8290@users.noreply.github.com>
2024-07-20 23:08:38 +0530

    page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
-import AddNewInterview from "./_components/AddNewInterview";
-import InterviewList from "./_components/interviewList";
+import AddNewInterview from "./_components/AddNewInterview.jsx";
+import InterviewList from "./_components/interviewList.jsx";
 
 function Dashboard(){
     return(
